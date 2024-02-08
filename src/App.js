2f93dfd5bee662d266e713c7bc5c12608dd6bc94
Devprakash Bisoi <56163787+DevprakashBisoi@users.noreply.github.com>
2024-02-08 11:57:01 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import About from "./components/About";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Portfolio from "./components/Skills";
+import SocialLinks from "./components/SocialLinks";
 
+
+function App() {
+  return (
+    <div className="app-container">
+      <NavBar />
+      <div className="section home-section">
+        <Home />
+      </div>
+      <div className="section about-section">
+        <About />
+      </div>
+      <div className="section experience-section">
+        <Experience />
+      </div>
+      <div className="section portfolio-section">
+        <Portfolio />
+      </div>
+      <div className="section contact-section">
+        <Contact />
+      </div>
+      <SocialLinks />
+    </div>
+  );
+}
+
+export default App;
