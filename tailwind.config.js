dc282093023983b68a71fc16f4dbbe1706d89a44
Devprakash Bisoi <56163787+DevprakashBisoi@users.noreply.github.com>
2024-02-08 11:53:20 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {},
+      fontFamily: {
+        signature: ["Press Start 2P", "monospace"],
+      },
+    },
+    plugins: [],
+  };
