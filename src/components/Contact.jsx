428ffedbaa6503152ca409934be8baf6f9d56411
Devprakash Bisoi <56163787+DevprakashBisoi@users.noreply.github.com>
2025-06-20 22:48:35 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,17 +5,18 @@ const Contact = () => {
     <div
       name="contact"
       className="w-full h-full text-white"
-      style={{ backgroundImage: "linear-gradient(to bottom, #080808, #368BC1)" }}
+      style={{
+        backgroundImage: "linear-gradient(to bottom, #080808, #368BC1)",
+      }}
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
-        <br></br> <br></br> 
+        <br></br> <br></br>
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Contact me
           </p>
           <p className="py-6">Submit the form below to get in touch with me</p>
         </div>
-
         <div className=" flex justify-center items-center">
           <form
             action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
@@ -46,9 +47,9 @@ const Contact = () => {
             </button>
           </form>
         </div>
-        <div className="flex justify-center items-center mt-5 text-sm text-white-500"> 
-      © 2022 - present Devprakash.
-      </div>
+        <div className="flex justify-center items-center mt-5 text-sm text-white-500">
+          © 2022 - present Devprakash.
+        </div>
       </div>
     </div>
   );
